f867f4436f2f720848aae207b5ebf6711d317c38
KostaGorod <korolx147@gmail.com>
2022-08-21 19:18:11 +0300

    fix misconfigure host fomr "www.ascii-art.de"

--- materials/vagrant/fixScripts/exercise2-fix.sh
+++ materials/vagrant/fixScripts/exercise2-fix.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 #add fix to exercise2 here
-sed '/^127.0.0.1 www.ascii-art.de/ s/./#&/' /etc/hosts
+echo "applying exercise2-fix.."
+sed -i -e '/^127.0.0.1 www.ascii-art.de/ s/./#&/' /etc/hosts
