0beb6365bbd2a563bcbbad4ea913c9dd1eda88dc
KostaGorod <korolx147@gmail.com>
2022-08-22 11:31:01 +0300

    fix "non-zero-code" in case of automations

--- materials/vagrant/fixScripts/exercise2-fix.sh
+++ materials/vagrant/fixScripts/exercise2-fix.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #add fix to exercise2 here
-echo "applying exercise2-fix.."	
+
+#echo "applying exercise2-fix.."	#<really wanted to add but scared of "non-zero" fatal error in automations
+
 #sed af
 sed -i -e '/^127.0.0.1 www.ascii-art.de/ s/./#&/' /etc/hosts
--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 #add fix to exercise3 here
-#Require all granted
-echo "applying exercise3-fix.."	
+
 #One time script, we know to precise location of the error,pattern match is an overkill.
+
+#echo "applying exercise3-fix.. #<really wanted to add but scared of "non-zero" fatal error in automations
+
+#Delete lines {32..34}
 gawk -i inplace 'NR!~/^(32|33|34)$/' /etc/apache2/sites-enabled/000-default.conf
 systemctl restart apache2.service
