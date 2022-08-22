ced791a78a285ee963e4acb0adbcec6ada84f032
KostaGorod <korolx147@gmail.com>
2022-08-22 11:06:11 +0300

    Fix "access Denied" for www/html apache

--- materials/vagrant/fixScripts/exercise2-fix.sh
+++ materials/vagrant/fixScripts/exercise2-fix.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 #add fix to exercise2 here
 echo "applying exercise2-fix.."	
+#sed af
 sed -i -e '/^127.0.0.1 www.ascii-art.de/ s/./#&/' /etc/hosts
--- materials/vagrant/fixScripts/exercise3-fix.sh
+++ materials/vagrant/fixScripts/exercise3-fix.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
 #add fix to exercise3 here
+#Require all granted
+echo "applying exercise3-fix.."	
+#One time script, we know to precise location of the error,pattern match is an overkill.
+gawk -i inplace 'NR!~/^(32|33|34)$/' /etc/apache2/sites-enabled/000-default.conf
+systemctl restart apache2.service
