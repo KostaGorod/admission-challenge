4772f198639a9c33ac983127e12d2d1adf99b2f1
KostaGorod <korolx147@gmail.com>
2022-08-22 14:32:56 +0300

    exercise 5 fix (don't judge, could be more secure)

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,2 +1,24 @@
 #!/bin/bash
 #add fix to exercise5-server1 here
+
+echo -e "\nHost server2\n    StrictHostKeyChecking no\n" >> /etc/ssh/ssh_config
+
+echo '-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+QyNTUxOQAAACDOdG5Y894uPIDNE3u4UHOlioFS9jeogPrQQxf+BvU7YQAAAJCrZhqWq2Ya
+lgAAAAtzc2gtZWQyNTUxOQAAACDOdG5Y894uPIDNE3u4UHOlioFS9jeogPrQQxf+BvU7YQ
+AAAEBG95BG6vL7X/nRjluYFxOVuNjzyBqq4+7y+oF3cLHJ5M50bljz3i48gM0Te7hQc6WK
+gVL2N6iA+tBDF/4G9TthAAAAB3NlcnZlcjEBAgMEBQY=
+-----END OPENSSH PRIVATE KEY-----
+' >> /root/.ssh/id_ed25519
+chmod 600 /root/.ssh/id_ed25519
+
+echo '-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAAAMwAAAAtzc2gtZW
+QyNTUxOQAAACDOdG5Y894uPIDNE3u4UHOlioFS9jeogPrQQxf+BvU7YQAAAJCrZhqWq2Ya
+lgAAAAtzc2gtZWQyNTUxOQAAACDOdG5Y894uPIDNE3u4UHOlioFS9jeogPrQQxf+BvU7YQ
+AAAEBG95BG6vL7X/nRjluYFxOVuNjzyBqq4+7y+oF3cLHJ5M50bljz3i48gM0Te7hQc6WK
+gVL2N6iA+tBDF/4G9TthAAAAB3NlcnZlcjEBAgMEBQY=
+-----END OPENSSH PRIVATE KEY-----
+' >> /home/vagrant/.ssh/id_ed25519
+chmod 600 /home/vagrant/.ssh/id_ed25519
--- materials/vagrant/fixScripts/exercise5-fix_server2.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server2.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 #add fix to exercise5-server2 here
+#    IdentityFile /root/ssh/ssh_server1.pub
+echo -e "\nHost server1\n    StrictHostKeyChecking no\n" >> /etc/ssh/ssh_config
+
+echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM50bljz3i48gM0Te7hQc6WKgVL2N6iA+tBDF/4G9Tth server1' >> /root/.ssh/authorized_keys
+chmod 644 /root/.ssh/authorized_keys
+
+echo 'ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM50bljz3i48gM0Te7hQc6WKgVL2N6iA+tBDF/4G9Tth server1' >> /home/vagrant/.ssh/authorized_keys
+chmod 644 /home/vagrant/.ssh/authorized_keys 
